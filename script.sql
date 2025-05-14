26a02f3fdc14a87522fe76f0f5dbd8b63cf6ffc7
chinenye13 <chinenyeagughasi13@gmail.com>
2025-05-14 16:43:41 +0200

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,14 +1,33 @@
+CREATE TABLE users(
+    user_id INTEGER NOT NULL,
+    full_name TEXT NOT NULL,
+    age INTEGER NOT NULL, 
+    medications TEXT,
+    health_issues TEXT,
+    PRIMARY KEY(user_id)
+);
+
 CREATE TABLE sensor_data(
+    user_id INTEGER FOREIGN KEY REFERENCES users(user_id),
 	longitude REAL,
 	latitude REAL,
 	bpm INTEGER,
-	date_time TIMESTAMP NOT NULL,
+	date_time DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	audio_id INTEGER NOT NULL,
-	CONSTRAINT date_time PRIMARY KEY(date_time)
+    accelerometer_id INTEGER NOT NULL,
+	PRIMARY KEY(date_time, user_id)
 );
 
 CREATE TABLE audio_data(
 	audio_id INTEGER FOREIGN KEY REFERENCES sensor_data(audio_id),
 	audio_data INTEGER,
-	CONSTRAINT audio_id PRIMARY KEY(audio_id)
+	PRIMARY KEY(audio_id)
+);
+
+CREATE TABLE accelerometer_data(
+    accelerometer_id INTEGER FOREIGN KEY REFERENCES sensor_data(accelerometer_id),
+    x REAL, 
+    y REAL,
+    z REAL,
+    PRIMARY KEY(accelerometer_id)
 );
