18038478bcf32ef7ce1a0f1f2b5a49e03b7a302f
chinenye13 <chinenyeagughasi13@gmail.com>
2025-05-14 16:01:45 +0200

    first look at the sql script. #todo figure out audio_id

---
+++ script.sql
@@ -0,0 +1,14 @@
+CREATE TABLE sensor_data(
+	longitude FLOAT,
+	latitude FLOAT,
+	bpm INTEGER,
+	date_time TIMESTAMP NOT NULL,
+	audio_id INTEGER NOT NULL,
+	CONSTRAINT date_time PRIMARY KEY(date_time)
+);
+
+CREATE TABLE audio_data(
+	audio_id INTEGER FOREIGN KEY REFERENCES sensor_data(audio_id),
+	audio_data INTEGER,
+	CONSTRAINT audio_id PRIMARY KEY(audio_id)
+);
